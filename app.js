3a84b00d43eed2b0482214e0f93fe3dd19745c34
Surjit <surjit_sen@yahoo.com>
2016-09-11 13:27:09 -0400

    Updated after feedback

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ angular.module('Module1Coding', [])
 
 .controller('Module1CodingController', function ($scope) {
   $scope.name = "";
-  $scope.message = "Please enter data first";
+  $scope.message = "";
 
   $scope.displayHowMuch = function () {
     var totalItems = calculateNumberOfItems($scope.name);
@@ -26,7 +26,7 @@ angular.module('Module1Coding', [])
     if (str === '')
       return 0;
     else {
-      return str.split(" ").length;
+      return str.split(",").length;
     }
   }
 
